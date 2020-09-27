cac17bc3c33e82f5da5418e63e839f8785999adc
Bricktech2000 <Bricktech2000@gmail.com>
2020-09-27 17:30:12 -0400

    restructured functions

---
+++ Parser.js
@@ -0,0 +1,22 @@
+Parser = {
+  parse = async function(text){
+    settings.log('Parsing...');
+    await sleep(1);
+    var dataHeader;
+    var obj = CSVParser.parse(text, function(obj, arr){
+      if(arr[0] == 'loopIteration'){
+        dataHeader = arr;
+        obj.data = [];
+        return;
+      }
+      if(dataHeader){
+        item = {};
+        for(var h = 0; h < dataHeader.length; h++)
+          item[dataHeader[h]] = arr[h];
+        obj.data.push(item);
+      }else obj[arr[0]] = arr[1]; //arr.slice(1);
+    });
+    //console.log(text);
+    return obj;
+  }
+}
