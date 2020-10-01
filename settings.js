925c39377972ac157e1411f9506eb9de654c0cc4
Bricktech2000 <Bricktech2000@gmail.com>
2020-09-30 21:42:33 -0400

    changed canvas resolution to 1080p

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 settings.fps = 30;
 settings.movingAverageDuration = .1;
-settings.height = 900;
-settings.width = 200;
-settings.padding = 10;
-settings.borderRadius = 10;
+settings.height = 1080;
+settings.width = 1080 * 2 / 9;
+settings.padding = 12;
+settings.borderRadius = 12;
 settings.background = '#222';
 settings.background2 = '#333';
 settings.color = '#f33';
 settings.accent = '#fff';
-settings.font = '20px arial';
+settings.font = '24px arial';
 settings.log = function(m){
   console.log(m);
   var div = document.createElement('div');
