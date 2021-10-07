ba9b9d0035855d4401a755060510ab8551344b0c
Bricktech <Bricktech2000@gmail.com>
2021-10-07 16:59:27 -0400

    prettified code

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 settings.fps = 30;
-settings.movingAverageDuration = .1;
+settings.movingAverageDuration = 0.1;
 settings.height = 1080;
-settings.width = 1080 * 2 / 9;
+settings.width = (1080 * 2) / 9;
 settings.padding = 12;
 settings.borderRadius = 12;
 settings.background = '#000';
@@ -12,9 +12,9 @@ settings.font = '24px arial';
 settings.cameraAngle = 0;
 settings.cameraFOV = 70;
 settings.fpvAspectRatio = 4 / 3; //4:3 aspect ratio
-settings.log = function(m){
+settings.log = function (m) {
   console.log(m);
   var div = document.createElement('div');
   div.textContent = m + '\n';
   settings.logDiv.appendChild(div);
-}
+};
